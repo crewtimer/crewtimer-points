0abe6a0db44d5fd7f5a31826f4707421915e9e8f
Glenn Engel <glenne@engel.org>
2026-06-26 13:09:04 -0700

    Tweak styling for Stars and Stripes

--- src/components/StarsAndStripesPoints.tsx
+++ src/components/StarsAndStripesPoints.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import { TableBody, TableCell, TableHead, TableRow } from '@mui/material';
 import { Results } from 'crewtimer-common';
 import { starsAndStripesPointsCalc } from '../calculators/StarsAndStripesPointsCalc';
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+const HeaderTableCell = styled(TableCell)(() => ({
+  fontWeight: 'bold',
+}));
 
 /**
  * Render team points for the Stars and Stripes Regatta, based on the
@@ -15,18 +29,18 @@ export const StarsAndStripesPoints: React.FC<{ results: Results }> = ({ results 
     <Table>
       <TableHead>
         <TableRow>
-          <TableCell>Place</TableCell>
-          <TableCell>Team Name</TableCell>
-          <TableCell>Points</TableCell>
+          <HeaderTableCell>Place</HeaderTableCell>
+          <HeaderTableCell>Team Name</HeaderTableCell>
+          <HeaderTableCell>Points</HeaderTableCell>
         </TableRow>
       </TableHead>
       <TableBody>
         {points.map((teamPoints) => (
-          <TableRow key={teamPoints.team}>
+          <StyledTableRow key={teamPoints.team}>
             <TableCell>{teamPoints.place}</TableCell>
             <TableCell>{teamPoints.team}</TableCell>
             <TableCell>{teamPoints.points}</TableCell>
-          </TableRow>
+          </StyledTableRow>
         ))}
       </TableBody>
     </Table>
